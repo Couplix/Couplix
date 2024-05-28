5ed568748293133a4569ab796bca9bbe956ce7e3
raipen <raipendalk@gmail.com>
2024-05-28 16:33:34 +0900

    feat: box changer

--- frontend/src/Router.tsx
+++ frontend/src/Router.tsx
@@ -1,12 +1,15 @@
 import { BrowserRouter,Routes, Route } from "react-router-dom";
 import Home from "./page/Home";
+import Box from "./page/Box";
+import BoxChange from "./page/BoxWithChange";
 
 const Router = () => {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<Home />} />
-                <Route path="/about" element={<h1>About</h1>} />
+                <Route path="/box" element={<Box />} />
+                <Route path="/boxChange" element={<BoxChange />} />
 
                 <Route path="*" element={<h1>Not Found</h1>} />
             </Routes>
