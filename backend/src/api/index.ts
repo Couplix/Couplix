6f43c4e399ca0f19f745c189bd849bdae7053ff2
gangin0221 <gangin0221@naver.com>
2024-06-07 15:21:10 +0900

    rate

--- backend/src/api/index.ts
+++ backend/src/api/index.ts
@@ -2,12 +2,15 @@ import {Router, Request, Response, NextFunction} from 'express';
 import test from './test';
 import contents from './contents';
 import categories from './categories'
+import rates from './rates'
+
 import errorHandler from '../utils/errorHandler';
 
 const app = Router();
 app.use('/', test);
 app.use('/contents', contents);
 app.use('/categories',categories);
+app.use('/rates', rates);
 
 app.use(errorHandler);
   
