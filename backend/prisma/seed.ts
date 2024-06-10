25ffdc53395b4e2ac9dda80810af9e40e6b4dedf
raipen <raipendalk@gmail.com>
2024-06-11 03:07:19 +0900

    feat: Extract categories
    The code changes extract the categories from the category.json file and add them to the database using the category model. This change improves the organization and management of categories in the application.

--- backend/prisma/seed.ts
+++ backend/prisma/seed.ts
@@ -1,12 +1,36 @@
 import { PrismaClient } from "@prisma/client";
+import categories from "./category.json";
 import seed from "./seed.json";
 
 const prisma = new PrismaClient();
 
 async function main() {
-    await prisma.netflix.createMany({
-        data: seed
+    await prisma.category.createMany({
+        data: categories.map((category) => {
+            return {
+                name: category
+            }
+        })
     });
+
+    for( let item of seed) {
+        await prisma.netflix.create({
+            data: {
+                ...{
+                    ...item,
+                    id: undefined,
+                    listed_in: undefined
+                },
+                categories: {
+                    connect: item.listed_in.split(", ").map((category) => {
+                        return {
+                            name: category
+                        }
+                    })
+                }
+            }
+        });
+    }
 }
 
 main()
