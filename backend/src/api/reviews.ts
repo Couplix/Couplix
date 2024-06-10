1704aa73da163a24e7b0cab040500530544dceb1
raipen <raipendalk@gmail.com>
2024-06-10 22:38:20 +0900

    feat: Refactor postReviews function in reviews.service
    The code changes refactor the postReviews function in the reviews.service.ts file. The function now handles the validation of input data and throws a ValidationError if the data is invalid. It also updates the database schema to use the correct field names for content and netflixId. This change improves the reliability and maintainability of the code.

--- backend/src/api/reviews.ts
+++ backend/src/api/reviews.ts
@@ -4,15 +4,10 @@ import * as reviewsService from '../services/reviews.service';
 
 const router = Router();
 
-router.get('/:id', errorCatcher(async (req: Request, res: Response) => {
-    const result = await reviewsService.getReviews(req.params.id);
-    res.status(200).json(result);
-}));
-
-router.post('/', async (req: Request, res: Response) => {
+router.post('/',errorCatcher(async (req: Request, res: Response) => {
   const result = await reviewsService.postReviews(req.body);
   res.status(201).json(result);
-});
+}));
   
 
 export default router;
--- backend/src/services/reviews.service.ts
+++ backend/src/services/reviews.service.ts
@@ -1,51 +1,23 @@
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
-import { Request, Response } from 'express';
+import { ValidationError } from "../errors";
 
-export const getReviews = async (id: string) => {
-    return await prisma.netflix.findUnique({
-        where: {
-            id: parseInt(id)
-        },
-        include: {
-            // title: true,
-            // director: true,
-            // cast: true,
-            // releaseYear: true,
-            // rating: true,
-            // duration: true,
-            // description: true,
-            // starRating: true
-        }
-    });
-}
-
-
-
-
-interface ReviewInput {
-    contentId: number;
-    review: string;
-}
-
-export const postReviews = async (req: Request, res: Response) => {
-    const { contentId, review }: ReviewInput = req.body as ReviewInput;
-    
-    if (!contentId || typeof contentId !== 'number' || !review || typeof review !== 'string') {
-        throw new Error('Invalid input data');
+export const postReviews = async ({ netflixId, review }: any) => {
+    if (!netflixId || typeof netflixId !== 'number' || !review || typeof review !== 'string') {
+        throw new ValidationError();
     }
     
     await prisma.review.create({
         data: {
-            contentId,
-            review
+            content: review,
+            netflixId
         }
     });
     
     const reviews = await prisma.review.findMany({
-        where: { contentId },
-        select: { review: true }
+        where: { netflixId },
+        select: { content: true }
     });
     
-    return reviews.map(r => r.review);
+    return reviews.map(r => r.content);
 }
